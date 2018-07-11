ee4db19334cbbfc0f00b296da98cd1327b7ef8b9
kr153 <rahul153@gmail.com>
2018-07-11 15:32:13 +0530

    test

--- js/app.js
+++ js/app.js
@@ -4,11 +4,11 @@ app.controller('emp',['$scope', 'calcFactory',  function($scope , calcFactory){
     $scope.b = 0;
     $scope.doSum = function(){
         $scope.sum = parseInt(+$scope.a) + parseInt(+$scope.b);
-    }
+    };
 }]);
 
 app.factory('calcFactory', [function(){
-    console.log('instat')
+    console.log('instat');
     var oCalcService = {};
     oCalcService.getSum = function(a,b){
     };
